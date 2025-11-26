7c8a44a18af7b7d45e5146e3d441949a6056f6c6
sanzortal <sanaazorrouktalidi@gmail.com>
2025-11-26 12:09:59 +0100

    Primera union
    Añadido el movimiento del jugador mas el enemigo del ojo humilde, mas escenas.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,225 @@
+
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] float movementSpeed;
+    private float initialSpeed;
+    private float maxSpeed;
+    [SerializeField] float aceleration;
+    [SerializeField] float rotationSpeed;
+    private bool isMoving;
+
+    //Control keys
+    [SerializeField] Key fowardKey;
+    [SerializeField] Key backwardKey;
+    [SerializeField] Key leftKey;
+    [SerializeField] Key rightKey;
+
+    //Jump
+    [SerializeField] float jumpForce;
+    private bool inAir;
+
+    //crouch
+    private bool isCrouching;
+    [SerializeField] float crouchSpeed;
+
+    //slide
+    private Vector3 slideVector;
+    [SerializeField] float slideForce;
+
+    //RigidBody
+    private Rigidbody rb;
+
+    //Collider
+    private BoxCollider bc;
+    private Vector3 initSize;
+    private Vector3 initCenter;
+
+    private void Start()
+    {
+        rb = this.gameObject.GetComponent<Rigidbody>();
+        bc = this.gameObject.GetComponent<BoxCollider>();
+        initSize = bc.size;
+        initCenter = bc.center;
+        initialSpeed = movementSpeed;
+        maxSpeed = movementSpeed + 4;
+        isCrouching = false;
+        isMoving = false;
+        slideVector = Vector3.zero;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        slideVector = Move();
+
+        if (Keyboard.current.leftShiftKey.isPressed && isMoving && !inAir)
+        {
+            Run();
+
+            if (Keyboard.current.leftCtrlKey.wasPressedThisFrame && !inAir && movementSpeed > initialSpeed + 2f)
+            {
+               slide(slideVector);
+            }
+        }
+        
+        if(Keyboard.current.leftShiftKey.wasReleasedThisFrame)
+        {
+            crouched();
+        }
+
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && !inAir && !isCrouching)
+        {
+            Jump();
+        }
+
+        if (Keyboard.current.leftCtrlKey.wasPressedThisFrame && !inAir) //preguntar que se quiere, si que si le da en el aire despues se agache o lo tenga que soltar y volverle a dar
+        {
+            Crouch();
+        }
+        else if (Keyboard.current.leftCtrlKey.wasReleasedThisFrame)
+        {
+            StandUp();
+        }
+
+    }
+
+    Vector3 Move()
+    {
+        Vector3 moveDirection = CalculateMoveDirection();
+
+        transform.position = transform.position + moveDirection * movementSpeed * Time.deltaTime;
+
+        if (moveDirection.magnitude != 0)
+        {
+            LookAt(moveDirection);
+
+            return moveDirection;
+        }
+        else
+        {
+            return Vector3.zero;
+        }
+    }
+
+    void LookAt(Vector3 lookDirection)
+    {
+        Quaternion targetRotation;
+        targetRotation = Quaternion.LookRotation(lookDirection); //Esto lo que hace es calcular la rotación que debería tener si estuviese girado para donde va realmente
+
+        Quaternion newRotation;
+        newRotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+        transform.rotation = newRotation;
+    }
+
+    Vector3 CalculateMoveDirection()
+    {
+        Vector3 moveVector;
+        Vector3 moveNormalized;
+
+        moveVector = new Vector3(0, 0, 0);
+
+        if (Keyboard.current[fowardKey].isPressed)
+        {
+            moveVector.z = moveVector.z + 1;
+        }
+
+        if (Keyboard.current[backwardKey].isPressed)
+        {
+            moveVector.z = moveVector.z - 1;
+        }
+
+        if (Keyboard.current[leftKey].isPressed)
+        {
+            moveVector.x = moveVector.x - 1;
+        }
+
+        if (Keyboard.current[rightKey].isPressed)
+        {
+            moveVector.x = moveVector.x + 1;
+        }
+
+        if (moveVector == Vector3.zero)
+        {
+            isMoving = false;
+
+            crouched();
+        }
+        else
+        {
+            isMoving = true;
+        }
+
+            moveNormalized = moveVector.normalized;
+
+        return moveNormalized;
+    }
+
+    void Jump()
+    {
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        inAir = true;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("floor"))
+        {
+            inAir = false;
+        }
+    }
+
+    private void Crouch()
+    {
+        //this.transform.localScale = new Vector3(this.transform.localScale.x, this.transform.localScale.y/2, this.transform.localScale.z);
+        bc.size = new Vector3(initSize.x, initSize.y / 2, initSize.z);
+        bc.center = new Vector3(initCenter.x, -(bc.size.y) / 2, initCenter.z);
+        movementSpeed = crouchSpeed;
+        isCrouching = true;
+    }
+
+    private void StandUp()
+    {
+        //this.transform.localScale = new Vector3(this.transform.localScale.x, this.transform.localScale.y * 2, this.transform.localScale.z);
+        bc.size = initSize;
+        bc.center = initCenter;
+        movementSpeed = initialSpeed;
+        isCrouching = false;
+    }
+
+    private void Run()
+    {
+        float max = maxSpeed;
+
+        if (isCrouching)
+        {
+            max = maxSpeed - (initialSpeed-crouchSpeed);
+        }
+
+        if (movementSpeed < max)
+        {
+            movementSpeed += movementSpeed * aceleration * Time.deltaTime;
+        }
+    }
+
+    void crouched()
+    {
+        if (!isCrouching)
+        {
+            movementSpeed = initialSpeed;
+        }
+        else
+        {
+            Crouch();
+        }
+    }
+
+    void slide(Vector3 moveDirection)
+    {
+        rb.AddForce(moveDirection * slideForce, ForceMode.Impulse);
+    }
+}
