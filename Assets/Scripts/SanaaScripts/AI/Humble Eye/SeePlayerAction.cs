377af7c4d4bf96c56fa9644ef441a791a6c9aa10
sanzortal <sanaazorrouktalidi@gmail.com>
2026-02-11 09:18:01 +0100

    asdfghjkhgfdsa

--- Assets/Scripts/SanaaScripts/AI/Humble Eye/SeePlayerAction.cs
+++ Assets/Scripts/SanaaScripts/AI/Humble Eye/SeePlayerAction.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using static UnityEngine.UI.Image;
 
 [CreateAssetMenu(fileName = "(a) SeePlayerAction", menuName = "ScriptableObjects/Actions/SeePlayerAction")]
 public class SeePlayerAction : DrawableAction
@@ -6,29 +7,37 @@ public class SeePlayerAction : DrawableAction
     [SerializeField] private float visionAngle;
     [SerializeField] private float radiusSphere;
 
+    [SerializeField] private LayerMask obstacleMask;
+    [SerializeField] private LayerMask playerMask;
+
     public override bool Check(GameObject owner)
     {
         PlayerController controller = FindFirstObjectByType<PlayerController>();
-        Vector3 dirToPlayer = (controller.transform.position - owner.transform.position).normalized;
+        if (controller == null) return false;
 
-        /*
-         * if (dirToPlayer.magnitude > radiusSphere)
+        Vector3 toPlayer = controller.transform.position - owner.transform.position;
+        float distance = toPlayer.magnitude;
+
+         if (distance > radiusSphere)
             return false;
-        */
 
+        Vector3 dirToPlayer = toPlayer.normalized;
+        
         float angle = Vector3.Angle(owner.transform.forward, dirToPlayer);
+        if (angle > visionAngle * 0.5f)
+            return false;
 
-        if (angle <= visionAngle / 2)
+        if (Physics.Raycast(owner.transform.position, dirToPlayer, out RaycastHit hit, distance, obstacleMask | playerMask))
         {
-            return true;
+            if (!hit.collider.CompareTag("Player"))
+                return false;
         }
-
-        return false;
+        return true;
     }
 
     public override void DrawGizmo(GameObject owner)
     {
-        Gizmos.color = Color.yellow;
+        Gizmos.color = Color.blue;
         Gizmos.DrawWireSphere(owner.transform.position, radiusSphere);
 
         Vector3 left = Quaternion.Euler(0f, -visionAngle / 2, 0f) * owner.transform.forward * radiusSphere;
